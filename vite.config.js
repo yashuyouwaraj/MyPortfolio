f8933f4b1cac869ddc0b417bf4d34c57c40bd06a
Yashu Youwaraj <amanyasu@gmail.com>
2025-11-24 22:07:11 +0530

    initial setup done

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "#components": resolve(
+        dirname(fileURLToPath(import.meta.url)),
+        "components"
+      ),
+      "#constants": resolve(
+        dirname(fileURLToPath(import.meta.url)),
+        "constants"
+      ),
+      "#store": resolve(dirname(fileURLToPath(import.meta.url)), "store"),
+      "#hoc": resolve(dirname(fileURLToPath(import.meta.url)), "hoc"),
+      "#windows": resolve(dirname(fileURLToPath(import.meta.url)), "windows"),
+    },
+  },
+});
