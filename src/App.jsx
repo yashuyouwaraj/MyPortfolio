359c701229c13afba59dab3f53533f63adaf7684
Yashu Youwaraj <amanyasu@gmail.com>
2025-11-25 09:13:21 +0530

    Navbar Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Navbar from "#components/Navbar"
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl">Welcome to my MacFolio</h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   )
 }
 
