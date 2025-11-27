257c3f0e34530fbff89a0852bd6401f2856c1fa4
Yashu Youwaraj <amanyasu@gmail.com>
2025-11-27 10:28:58 +0530

    Done deployment build

--- src/windows/Resume.jsx
+++ src/windows/Resume.jsx
@@ -10,6 +10,10 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   import.meta.url
 ).toString();
 
+//For deployment uncomment this and comment above line
+// pdfjs.GlobalWorkerOptions.workerSrc = `https:"//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+
+
 const Resume = () => {
   return (
     <>
